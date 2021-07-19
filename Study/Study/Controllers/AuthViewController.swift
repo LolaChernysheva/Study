bd84f74ca0a061bd062f698837b56cb2edd93bc8
LolaChernysheva <62766749+LolaChernysheva@users.noreply.github.com>
2021-07-19 22:05:19 +0300

    CodeStyle
    CodeStyle

--- Study/Study/Controllers/AuthViewController.swift
+++ Study/Study/Controllers/AuthViewController.swift
@@ -20,7 +20,6 @@ class AuthViewController: UIViewController {
 	private let signUpButton = UIButton(type: .system)
 	private let passwordTextField = UITextField()
 	
-	
     override func viewDidLoad() {
         super.viewDidLoad()
 		initialize()
@@ -36,7 +35,10 @@ class AuthViewController: UIViewController {
 	}
 	
 	private func initialize() {
-		view.backgroundColor = UIColor(red: 255/255, green: 254/255, blue: 254/255, alpha: 1)
+		view.backgroundColor = UIColor(red: 255/255,
+									   green: 254/255,
+									   blue: 254/255,
+									   alpha: 1)
 		view.addSubview(scrollView)
 		scrollView.snp.makeConstraints { maker in
 			maker.top.equalToSuperview()
@@ -55,7 +57,10 @@ class AuthViewController: UIViewController {
 			maker.top.equalToSuperview().inset(100)
 		}
 		
-		loginTextField.backgroundColor = UIColor(red: 241/255, green: 242/255, blue: 244/255, alpha: 1)
+		loginTextField.backgroundColor = UIColor(red: 241/255,
+												 green: 242/255,
+												 blue: 244/255,
+												 alpha: 1)
 		loginTextField.text = "Email или телефон"
 		loginTextField.font = UIFont.systemFont(ofSize: 16)
 		loginTextField.layer.cornerRadius = 5.0
@@ -68,7 +73,10 @@ class AuthViewController: UIViewController {
 			maker.top.equalTo(logoImageView).inset(150)
 		}
 		
-		passwordTextField.backgroundColor = UIColor(red: 241/255, green: 242/255, blue: 244/255, alpha: 1)
+		passwordTextField.backgroundColor = UIColor(red: 241/255,
+													green: 242/255,
+													blue: 244/255,
+													alpha: 1)
 		passwordTextField.text = "Пароль"
 		passwordTextField.font = UIFont.systemFont(ofSize: 16)
 		passwordTextField.layer.cornerRadius = 5.0
@@ -81,7 +89,10 @@ class AuthViewController: UIViewController {
 			maker.top.equalTo(loginTextField).inset(50)
 		}
 		
-		signInButton.backgroundColor = UIColor(red: 39/255, green: 135/255, blue: 245/255, alpha: 1)
+		signInButton.backgroundColor = UIColor(red: 39/255,
+											   green: 135/255,
+											   blue: 245/255,
+											   alpha: 1)
 		signInButton.setTitle("Войти", for: .normal)
 		signInButton.tintColor = .white
 		signInButton.layer.cornerRadius = 5.0
@@ -94,7 +105,10 @@ class AuthViewController: UIViewController {
 		}
 		
 		signUpButton.setTitle("Зарегистрироваться", for: .normal)
-		signUpButton.tintColor = UIColor(red: 39/255, green: 135/255, blue: 245/255, alpha: 1)
+		signUpButton.tintColor = UIColor(red: 39/255,
+										 green: 135/255,
+										 blue: 245/255,
+										 alpha: 1)
 		scrollView.addSubview(signUpButton)
 		signUpButton.snp.makeConstraints { maker in
 			maker.centerX.equalToSuperview()
@@ -110,19 +124,31 @@ class AuthViewController: UIViewController {
 			maker.centerX.equalToSuperview()
 			maker.centerY.equalToSuperview()
 		}
-		signInButton.addTarget(self, action: #selector(buttonPressed), for:.touchUpInside)
+		signInButton.addTarget(self,
+							   action: #selector(buttonPressed),
+							   for:.touchUpInside)
 	}
 	
 	//подписка под уведомления об изменении состояния клавиатуры
 	func registerForKeyboardNotifications() {
-		NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification , object: nil)
-		NotificationCenter.default.addObserver(self, selector: #selector(kbWillHide), name: UIResponder.keyboardWillHideNotification , object: nil)
+		NotificationCenter.default.addObserver(self,
+											   selector: #selector(kbWillShow),
+											   name: UIResponder.keyboardWillShowNotification ,
+											   object: nil)
+		NotificationCenter.default.addObserver(self,
+											   selector: #selector(kbWillHide),
+											   name: UIResponder.keyboardWillHideNotification ,
+											   object: nil)
 	}
 	
 	//отписка от уведомлений об изменении состояния клавиатуры
 	func removeKeyboardNotifications() {
-		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification , object: nil)
-		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification , object: nil)
+		NotificationCenter.default.removeObserver(self,
+												  name: UIResponder.keyboardWillShowNotification ,
+												  object: nil)
+		NotificationCenter.default.removeObserver(self,
+												  name: UIResponder.keyboardWillHideNotification ,
+												  object: nil)
 	}
 	
 	//при появлении клавиатуры
@@ -140,7 +166,10 @@ class AuthViewController: UIViewController {
 	
 	//обработка события нажатия на кнопку
 	@objc private func buttonPressed() {
-		signInButton.backgroundColor = UIColor(red: 181/255, green: 206/255, blue: 234/255, alpha: 1)
+		signInButton.backgroundColor = UIColor(red: 181/255,
+											   green: 206/255,
+											   blue: 234/255,
+											   alpha: 1)
 		signInButton.isEnabled = false
 		activityIndicatior.isHidden = false
 		loginTextField.isEnabled = false
