7fa03f3a81c3a689b38d0f5deeae097d12cc6f62
LolaChernysheva <62766749+LolaChernysheva@users.noreply.github.com>
2021-07-19 19:35:08 +0300

    Клавиатура, недоступность элементов UI по нажатию на кнопку "Войти"
    Добавила скрытие клавиатуры по нажатию на место вне контейнера
    
    Добавила недоступность элементов UI по нажатию на кнопку "Войти"

--- Study/Study/Controllers/AuthViewController.swift
+++ Study/Study/Controllers/AuthViewController.swift
@@ -4,15 +4,29 @@
 //
 //  Created by admin on 19.07.2021.
 //
+/*MARK: TODO
+- добавить скролл вью
+- добавить клавиатуру
+- добавить поднятие интерфейся на высоту клавиатуры
+- заменить размеры констрейнтов
+*/
 
 import SnapKit
 import UIKit
 
 class AuthViewController: UIViewController {
+	
 	private let signInButton = UIButton(type: .system)
+	private let activityIndicatior = UIActivityIndicatorView()
+	private let loginTextField = UITextField()
+	private let passwordTextField = UITextField()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 		
+		//скрытие клавиатуры по нажатию на контейнер
+		let hideAction = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+		view.addGestureRecognizer(hideAction)
 		initialize()
     }
 	
@@ -29,7 +43,6 @@ class AuthViewController: UIViewController {
 			maker.top.equalToSuperview().inset(100)
 		}
 		
-		let loginTextField = UITextField()
 		loginTextField.backgroundColor = UIColor(red: 241/255, green: 242/255, blue: 244/255, alpha: 1)
 		loginTextField.text = "Email или телефон"
 		loginTextField.font = UIFont.systemFont(ofSize: 16)
@@ -43,7 +56,6 @@ class AuthViewController: UIViewController {
 			maker.top.equalTo(logoImageView).inset(150)
 		}
 		
-		let passwordTextField = UITextField()
 		passwordTextField.backgroundColor = UIColor(red: 241/255, green: 242/255, blue: 244/255, alpha: 1)
 		passwordTextField.text = "Пароль"
 		passwordTextField.font = UIFont.systemFont(ofSize: 16)
@@ -57,8 +69,6 @@ class AuthViewController: UIViewController {
 			maker.top.equalTo(loginTextField).inset(50)
 		}
 		
-		//let okColor = UIColor(red: 39/255, green: 135/255, blue: 245/255, alpha: 1)
-		//let notOkColor = UIColor(red: 181/255, green: 206/255, blue: 234/255, alpha: 1)
 		signInButton.backgroundColor = UIColor(red: 39/255, green: 135/255, blue: 245/255, alpha: 1)
 		signInButton.setTitle("Войти", for: .normal)
 		signInButton.tintColor = .white
@@ -70,12 +80,28 @@ class AuthViewController: UIViewController {
 			maker.height.equalTo(30)
 			maker.top.equalTo(passwordTextField).inset(50)
 		}
+		
+		view.addSubview(activityIndicatior)
+		activityIndicatior.isHidden = true
+		activityIndicatior.snp.makeConstraints { maker in
+			maker.centerX.equalToSuperview()
+			maker.centerY.equalToSuperview()
+		}
 		signInButton.addTarget(self, action: #selector(buttonPressed), for:.touchUpInside)
-		
-		
 	}
 	
-	@objc func buttonPressed() {
+	@objc private func buttonPressed() {
 		signInButton.backgroundColor = UIColor(red: 181/255, green: 206/255, blue: 234/255, alpha: 1)
+		signInButton.isEnabled = false
+		activityIndicatior.isHidden = false
+		loginTextField.isEnabled = false
+		passwordTextField.isEnabled = false
+		view.alpha = 0.9
+		activityIndicatior.startAnimating()
+	}
+	
+	//скрытие клавиатуры по нажатию на контейнер
+	@objc private func hideKeyboard() {
+		view.endEditing(true)
 	}
 }
