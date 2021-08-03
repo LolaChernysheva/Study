52a6b6b24795189353792b3cdd54cdc6317169f7
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-08-03 17:40:49 +0300

    Добавлен кастомный цвет
    Добавлен кастомный цвет

--- Study/Study/CustomColors.swift
+++ Study/Study/CustomColors.swift
@@ -10,6 +10,7 @@ import UIKit
 enum AppAppearence {
 	static let backgroundColor = UIColor(red: 255/255, green: 254/255, blue: 254/255, alpha: 1)
 	static let textFieldBackgroundColor = UIColor(red: 241/255, green: 242/255, blue: 244/255, alpha: 1)
+    static let customBlue = UIColor(red: 39/255, green: 135/255, blue: 245/255, alpha: 1)
 }
 
 enum ButtonAppearance {
