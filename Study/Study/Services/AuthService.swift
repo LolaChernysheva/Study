57dbbe7ed9976d4334d8c9a3b4a08ebc0a9fb811
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-10-01 21:50:01 +0300

    Авторизация через VK SDK
    Создан класс, который отвечает за авторизацию с помощью VK SDK

---
+++ Study/Study/Services/AuthService.swift
@@ -0,0 +1,42 @@
+//
+//  AuthService.swift
+//  Study
+//
+//  Created by Лолита Чернышева on 01.10.2021.
+//
+
+import Foundation
+import VK_ios_sdk
+
+final class AuthService: NSObject, VKSdkDelegate, VKSdkUIDelegate {
+    
+    private let appId = "7965293"
+    private let vkSdk: VKSdk
+    
+    override init() {
+        vkSdk = VKSdk.initialize(withAppId: appId)
+        super.init()
+        vkSdk.register(self)
+        vkSdk.uiDelegate = self
+    }
+    
+    //MARK: VKSdkDelegate
+    
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+        print(#function)
+    }
+    
+    func vkSdkUserAuthorizationFailed() {
+        print(#function)
+    }
+    
+    //MARK: VKSdkUIDelegate
+    
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        print(#function)
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print(#function)
+    }
+}
