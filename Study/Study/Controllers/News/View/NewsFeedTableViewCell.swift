3d1da083118c7ab608f0ccc2c45b80447871f644
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-10-27 21:36:31 +0300

    Добавлен первый и второй слой элементов
    Добавлен первый и второй слой элементов

--- Study/Study/Controllers/News/View/NewsFeedTableViewCell.swift
+++ Study/Study/Controllers/News/View/NewsFeedTableViewCell.swift
@@ -11,9 +11,40 @@ final class NewsFeedTableViewCell: UITableViewCell {
     
     static let reusedId = "NewsFeedTableViewCell"
     
+    //Первый слой элементов
     let cardView: UIView = {
         let view = UIView()
         view.backgroundColor = .magenta
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    //Второй слой элементов
+    let topView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let postLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = Constants.postLabelFont
+        label.backgroundColor = .yellow
+        return label
+        
+    }()
+    
+    let postImageView: WebImageView = {
+        let imageView = WebImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .blue
+        return imageView
+    }()
+    
+    let bottomView: UIView = {
+        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
@@ -28,9 +59,15 @@ final class NewsFeedTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //Первый слой элементов
     private func overlayFirstLayer() {
         addSubview(cardView)
         cardView.fillSuperview(padding: Constants.cardInsets)
+    }
+    
+    //Второй слой элементов
+    private func overlaySecondLayer() {
+        
     }
     
 
