a7d3339c6db16c5fd32471a2477c194a02b01adc
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-10-27 11:52:26 +0300

    code style
    code style

--- Study/Study/NewsFeed/NewsFeedViewController.swift
+++ Study/Study/NewsFeed/NewsFeedViewController.swift
@@ -10,60 +10,62 @@ import UIKit
 import SnapKit
 
 protocol NewsFeedDisplayLogic: class {
-  func displayData(viewModel: NewsFeed.Model.ViewModel.ViewModelData)
+    func displayData(viewModel: NewsFeed.Model.ViewModel.ViewModelData)
 }
 
 class NewsFeedViewController: UIViewController, NewsFeedDisplayLogic {
     
     var tableView = UITableView()
-
-  var interactor: NewsFeedBusinessLogic?
-  var router: (NSObjectProtocol & NewsFeedRoutingLogic)?
-
-  // MARK: Object lifecycle
-  
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setup()
-      tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    setup()
-  }
-  
-  // MARK: Setup
-  
-  private func setup() {
-    let viewController        = self
-    let interactor            = NewsFeedInteractor()
-    let presenter             = NewsFeedPresenter()
-    let router                = NewsFeedRouter()
-    viewController.interactor = interactor
-    viewController.router     = router
-    interactor.presenter      = presenter
-    presenter.viewController  = viewController
-    router.viewController     = viewController
-  }
-  
-  // MARK: Routing
-  
-
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-      self.tableView.delegate = self
-      self.tableView.dataSource = self
-      
-      self.setUpConstraints()
-  }
-  
-  func displayData(viewModel: NewsFeed.Model.ViewModel.ViewModelData) {
-
-  }
+    
+    var interactor: NewsFeedBusinessLogic?
+    var router: (NSObjectProtocol & NewsFeedRoutingLogic)?
+    
+    // MARK: Object lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    
+    private func setup() {
+        let viewController        = self
+        let interactor            = NewsFeedInteractor()
+        let presenter             = NewsFeedPresenter()
+        let router                = NewsFeedRouter()
+        viewController.interactor = interactor
+        viewController.router     = router
+        interactor.presenter      = presenter
+        presenter.viewController  = viewController
+        router.viewController     = viewController
+    }
+    
+    // MARK: Routing
+    
+    
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.setUpConstraints()
+    }
+    
+    func displayData(viewModel: NewsFeed.Model.ViewModel.ViewModelData) {
+        
+    }
+    
+    
     
     private func setUpConstraints() {
         
