c95dfa0ae1888b46d51b54dd75b83db8e2718438
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-11-01 15:45:27 +0300

    Code style
    Code style

--- Study/Study/NewsFeed/NewsFeedInteractor.swift
+++ Study/Study/NewsFeed/NewsFeedInteractor.swift
@@ -9,18 +9,20 @@
 import UIKit
 
 protocol NewsFeedBusinessLogic {
-  func makeRequest(request: NewsFeed.Model.Request.RequestType)
+    func makeRequest(request: NewsFeed.Model.Request.RequestType)
 }
 
 class NewsFeedInteractor: NewsFeedBusinessLogic {
-
-  var presenter: NewsFeedPresentationLogic?
-  var service: NewsFeedService?
-  
-  func makeRequest(request: NewsFeed.Model.Request.RequestType) {
-    if service == nil {
-      service = NewsFeedService()
+    
+    var presenter: NewsFeedPresentationLogic?
+    var service: NewsFeedService?
+    
+    func makeRequest(request: NewsFeed.Model.Request.RequestType) {
+        if service == nil {
+            service = NewsFeedService()
+        }
+        
+        
     }
-  }
-  
+    
 }
