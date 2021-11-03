371ff6374863ba5b8e993511b6ea250c758e5f55
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-11-03 12:38:54 +0300

    codeStyle
    codeStyle

--- Study/Study/NewsFeed/NewsFeedPresenter.swift
+++ Study/Study/NewsFeed/NewsFeedPresenter.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 protocol NewsFeedPresentationLogic {
-  func presentData(response: NewsFeed.Model.Response.ResponseType)
+    func presentData(response: NewsFeed.Model.Response.ResponseType)
 }
 
 class NewsFeedPresenter: NewsFeedPresentationLogic {
