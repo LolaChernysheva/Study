c762ff5514a452f41c9d4e3ccf44fe0a1b3029ff
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-11-25 18:36:02 +0300

    Отступы текста и плейсхолдера
    Отступы текста и плейсхолдера

--- Study/Study/NewsFeed/ TitleView/InsetableTextField.swift
+++ Study/Study/NewsFeed/ TitleView/InsetableTextField.swift
@@ -33,6 +33,14 @@ class InsetableTextField: UITextField {
         return rect
     }
     
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 36, dy: 0)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 40, dy: 0)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
