0ac50518d6a20178d5c323d9fa9dade3dfeb246d
LolaChernysheva <lolita-chernysheva@mail.ru>
2021-12-01 15:48:20 +0300

    FooterView новостной ленты
    Добавлен класс FooterView для отображения нижней панели новостной ленты

---
+++ Study/Study/NewsFeed/FooterView/FooterView.swift
@@ -0,0 +1,21 @@
+//
+//  FooterView.swift
+//  Study
+//
+//  Created by Лолита Чернышева on 01.12.2021.
+//
+
+import Foundation
+import UIKit
+
+class FooterView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
